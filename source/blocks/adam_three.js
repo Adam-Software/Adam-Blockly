1be76435f5b10d3fcc036fef1a4d09a45f45cd82
Alexey Nesterov <vertigra.developer@gmail.com>
2023-05-22 15:57:49 +1000

    Added: block for servo controller command

--- source/blocks/adam_three.js
+++ source/blocks/adam_three.js
@@ -6,4 +6,243 @@
 
 Blockly.defineBlocksWithJsonArray([
 
+/**
+  * controller_motor_command
+  *
+  */
+{
+  "type": "controller_motor_command",
+  "message0": "команда %1 константа сервопривода %2 угол поворота %3 скорость %4",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "servo_constant"
+    },
+    {
+      "type": "input_value",
+      "name": "angle"
+    },
+    {
+      "type": "input_value",
+      "name": "speed"
+    }
+  ],
+  "output": null,
+  "colour": 180,
+  "tooltip": "Блок управление сервоприводами робота",
+  "helpUrl": ""
+},
+
+/**
+  * servo_speed_variable
+  *
+  */
+{
+  "type": "controller_speed_variable",
+  "message0": "скорость %1",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "speed",
+      "value": 0,
+      "min": 0,
+      "max": 2000,
+      "precision": 1
+    }
+  ],
+  "inputsInline": false,
+  "output": "Number",
+  "colour": 180,
+  "tooltip": "Переменная скорости. Может иметь значение от 0 до 2000",
+  "helpUrl": ""
+},
+
+/**
+  * servo_angle_variable
+  *
+  */
+{
+  "type": "controller_angle_variable",
+  "message0": "угол %1 %%",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "angle",
+      "value": 0,
+      "min": 0,
+      "max": 100,
+      "precision": 1
+    }
+  ],
+  "inputsInline": false,
+  "output": "Number",
+  "colour": 180,
+  "tooltip": "Переменная угла в процентах. Может иметь значение от 0 до 100",
+  "helpUrl": ""
+},
+
+/**
+  * controler_const_head
+  *
+  */
+{
+  "type": "controler_const_head",
+  "message0": "голова",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controler_const_neck
+  *
+  */
+{
+  "type": "controler_const_neck",
+  "message0": "шея",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controler_const_right_hand
+  *
+  */
+{
+  "type": "controler_const_right_hand",
+  "message0": "правая рука",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controler_const_left_hand
+  *
+  */
+{
+  "type": "controler_const_left_hand",
+  "message0": "левая рука",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controller_right_lower_arm_up
+  *
+  */
+{
+  "type": "controller_right_lower_arm_up",
+  "message0": "правый локоть",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controller_left_lower_arm_up
+  *
+  */
+{
+  "type": "controller_left_lower_arm_up",
+  "message0": "левый локоть",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controller_right_upper_arm
+  *
+  */
+{
+  "type": "controller_right_upper_arm",
+  "message0": "правое предплечье",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controller_left_upper_arm
+  *
+  */
+{
+  "type": "controller_left_upper_arm",
+  "message0": "левое предплечье",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controller_right_shoulder
+  *
+  */
+{
+  "type": "controller_right_shoulder",
+  "message0": "правое плечо",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controller_left_shoulder
+  *
+  */
+{
+  "type": "controller_left_shoulder",
+  "message0": "левое плечо",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controller_chest
+  *
+  */
+{
+  "type": "controller_chest",
+  "message0": "грудь",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+
+/**
+  * controller_lover_chest
+  *
+  */
+{
+  "type": "controller_lover_chest",
+  "message0": "пресс",
+  "output": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+}
+
+
+
+
+
+
 ]);
--- source/generators/python/adam_three.js
+++ source/generators/python/adam_three.js
@@ -6,3 +6,91 @@
 'use strict';
 
 Blockly.Python.addReservedWords('');
+
+Blockly.Python['controller_motor_command'] = function(block) {
+  var value_servo_constant = Blockly.Python.valueToCode(block, 'servo_constant', Blockly.Python.ORDER_ATOMIC);
+  var value_angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
+  var value_speed = Blockly.Python.valueToCode(block, 'speed', Blockly.Python.ORDER_ATOMIC);
+  var code = 'MotorCommand(' + "'" + value_servo_constant + "', " + value_angle + ', ' + value_speed + ')';
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['controler_const_head'] = function(block) {
+  var code = 'Head';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controler_const_neck'] = function(block) {
+  var code = 'Neck';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controler_const_right_hand'] = function(block) {
+  var code = 'Right_Hand';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controler_const_left_hand'] = function(block) {
+  var code = 'Left_Hand';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controller_right_lower_arm_up'] = function(block) {
+  var code = 'Right_LowerArm_Up';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controller_left_lower_arm_up'] = function(block) {
+  var code = 'Left_LowerArm_Up';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controller_right_upper_arm'] = function(block) {
+  var code = 'Right_UpperArm';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controller_left_upper_arm'] = function(block) {
+  var code = 'Left_UpperArm';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controller_right_shoulder'] = function(block) {
+  var code = 'Right_Shoulder';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controller_left_shoulder'] = function(block) {
+  var code = 'Left_Shoulder';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controller_chest'] = function(block) {
+  var code = 'Chest';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['controller_lover_chest'] = function(block) {
+  var code = 'LoverChest';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+/**
+ * servo_speed_variable
+ *
+ */
+Blockly.Python['controller_speed_variable'] = function(block) {
+  var number_speed = block.getFieldValue('speed');
+  var code = number_speed;
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+/**
+ * servo_angle_variable
+ *
+ */
+Blockly.Python['controller_angle_variable'] = function(block) {
+  var number_angle = block.getFieldValue('angle');
+  var code = number_angle;
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
